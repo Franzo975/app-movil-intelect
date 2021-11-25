809e7d6818d4896677a988063481c3e17e3b61a2
Roberto <irobertuber@gmail.com>
2021-11-25 11:10:50 -0500

    init

---
+++ app/src/main/java/pe/edu/usat/hmera/laboratorio/serviciosweb/logica/Sesion.java
@@ -0,0 +1,11 @@
+package pe.edu.usat.hmera.laboratorio.serviciosweb.logica;
+
+public class Sesion {
+    public static String NOMBRE;
+    public static String EMAIL;
+    public static int ID;
+    public static String FOTO;
+    public static String TOKEN;
+
+}
+
